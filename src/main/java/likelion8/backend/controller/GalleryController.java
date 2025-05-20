2cfbb9a617f9227c7ac1ce133f6f09ee4a1298a9
mongsam2 <songjaehyun121476@gmail.com>
2025-05-20 21:31:46 +0900

    chore: 주석 추가

--- src/main/java/likelion8/backend/controller/GalleryController.java
+++ src/main/java/likelion8/backend/controller/GalleryController.java
@@ -5,10 +5,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/galleries")
-@RequiredArgsConstructor
+@RestController //RESTfull 웹 서비스를 만들 때 사용하는 어노테이션. html페이지가 아니라 api 응답을 주고받는 컨트롤러
+@RequestMapping("/api/galleries") //클래스에 붙일 경우 공통 URL 역할. 아래 메소드들은 이 경로로 시작되는 요청들을 받음
+@RequiredArgsConstructor //아래 final로 선언된 필드들만 골라서 생성자를 자동으로 생성!
 public class GalleryController {
 
-    private final GalleryService galleryService;
+    private final GalleryService galleryService; // (컨트롤러 -> 서비스 -> 레포지토리) 컨트롤러에서는 서비스를 사용해서 로직 작성
+
+    // Lombok의 RequiredArgsConstructor가 아래와 같은 생성자를 만들어 줌
+//    public GalleryController(GalleryService galleryService) {
+//        this.galleryService = galleryService;
+//    }
 }
--- src/main/java/likelion8/backend/repository/GalleryRepository.java
+++ src/main/java/likelion8/backend/repository/GalleryRepository.java
@@ -3,5 +3,8 @@ package likelion8.backend.repository;
 import likelion8.backend.domain.Gallery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// "엔티티를 DB와 주고받는 역할을 담당하는 계층이다." "데이터베이스와 소통하는 창구"
+// JpaRepository를 상속받는 이유? -> 직접 구현하지 않아도 Spring Data JPA가 내부에서 구현체를 만들어서 자동으로 Bean으로 등록
+// 어떤 엔티티를 다루는지, 엔티티의 pk 데이터타입이 뭔지 전달해주면 됨
 public interface GalleryRepository extends JpaRepository<Gallery, Long> {
 }
