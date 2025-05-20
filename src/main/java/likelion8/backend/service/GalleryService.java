065384068724126d671463fcbd07d576b8de083a
mongsam2 <songjaehyun121476@gmail.com>
2025-05-20 22:04:22 +0900

    feat: 모든 Gallery 인스턴스 조회하는 service 메소드 작성

--- src/main/java/likelion8/backend/service/GalleryService.java
+++ src/main/java/likelion8/backend/service/GalleryService.java
@@ -1,12 +1,23 @@
 package likelion8.backend.service;
 
+import likelion8.backend.domain.Gallery;
+import likelion8.backend.dto.GalleryResponseDto;
 import likelion8.backend.repository.GalleryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service // 서비스 클래스임을 나타내는 어노테이션
 @RequiredArgsConstructor //아래 final로 선언된 필드들만 골라서 생성자를 자동으로 생성!
 public class GalleryService {
 
     private final GalleryRepository galleryRepository; // (컨트롤러 -> 서비스 -> 레포지토리) 서비스에서는 레포지토리를 사용해서 로직 작성
+
+    public List<GalleryResponseDto> getAllGalleries() {
+        List<Gallery> galleries = galleryRepository.findAll();
+        return galleries.stream().map(
+                gallery -> new GalleryResponseDto(gallery)
+        ).toList();
+    }
 }
