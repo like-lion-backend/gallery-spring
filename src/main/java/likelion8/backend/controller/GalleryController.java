913ecbd59be25906a77aeedc1ba0e5cf15088396
mongsam2 <songjaehyun121476@gmail.com>
2025-05-20 22:06:45 +0900

    feat: GET /api/galleries 컨트롤러 작성

--- src/main/java/likelion8/backend/controller/GalleryController.java
+++ src/main/java/likelion8/backend/controller/GalleryController.java
@@ -1,9 +1,14 @@
 package likelion8.backend.controller;
 
+import likelion8.backend.dto.GalleryResponseDto;
 import likelion8.backend.service.GalleryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController //RESTfull 웹 서비스를 만들 때 사용하는 어노테이션. html페이지가 아니라 api 응답을 주고받는 컨트롤러
 @RequestMapping("/api/galleries") //클래스에 붙일 경우 공통 URL 역할. 아래 메소드들은 이 경로로 시작되는 요청들을 받음
@@ -11,6 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class GalleryController {
 
     private final GalleryService galleryService; // (컨트롤러 -> 서비스 -> 레포지토리) 컨트롤러에서는 서비스를 사용해서 로직 작성
+
+    @GetMapping
+    public ResponseEntity<List<GalleryResponseDto>> getGalleries() {
+        List<GalleryResponseDto> galleries = galleryService.getAllGalleries();
+        return ResponseEntity.ok(galleries);
+    }
+
 
     // Lombok의 RequiredArgsConstructor가 아래와 같은 생성자를 만들어 줌
 //    public GalleryController(GalleryService galleryService) {
